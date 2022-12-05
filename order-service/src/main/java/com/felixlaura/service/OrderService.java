1bad5ea2c22dbd3bf079ae7db42d293f05b6be06
Felix Laura <felixgato8@gmail.com>
2022-12-04 22:55:29 -0500

    gitignore

--- order-service/src/main/java/com/felixlaura/service/OrderService.java
+++ order-service/src/main/java/com/felixlaura/service/OrderService.java
@@ -1,13 +1,18 @@
 package com.felixlaura.service;
 
+import brave.Span;
+import brave.Tracer;
 import com.fasterxml.jackson.databind.ser.std.UUIDSerializer;
 import com.felixlaura.dto.InventoryResponse;
 import com.felixlaura.dto.OrderLineItemsDto;
 import com.felixlaura.dto.OrderRequest;
 import com.felixlaura.model.Order;
 import com.felixlaura.model.OrderLineItems;
+import com.felixlaura.order.OrderPlacedEvent;
 import com.felixlaura.repository.OrderRepository;
+import io.vavr.collection.Tree;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,41 +29,52 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public String placeOrder(OrderRequest orderRequest){
+    private final Tracer tracer;
 
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
-                .map(orderLineItemsDto -> mapToDto(orderLineItemsDto)).toList();
+                .map(this::mapToDto)
+                .toList();
 
         order.setOrderLineItemsList(orderLineItems);
 
-        //collect all skuCodes from Order Object
         List<String> skuCodes = order.getOrderLineItemsList().stream()
-                .map(orderLineItem -> orderLineItem.getSkuCode())
+                .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        //Call Inventory Service, and place an order if product is in stock
-        //Make a get call to inventory service.
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class) //return type controller inventory service
-                .block();
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+        try (Tracer.SpanInScope isLookup = tracer.withSpanInScope(((Span) inventoryServiceLookup).start())) {
 
-        if(allProductsInStock){
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        }else {
-            throw new IllegalArgumentException("Product is not in stock, please try again later");
+            inventoryServiceLookup.tag("call", "inventory-service");
+            // Call Inventory Service, and place order if product is in
+            // stock
+            InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if (allProductsInStock) {
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        } finally {
+            inventoryServiceLookup.flush();
         }
-
-
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
